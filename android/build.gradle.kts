7a217d8591d310a0075d962e69202a94e156a6b2
Long Duc <duc.long@sotatek.com>
2026-01-18 21:31:57 +0700

    config ios

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -5,6 +5,12 @@ allprojects {
     }
 }
 
+plugins {
+    id("com.android.application") version "8.1.0" apply false
+    id("com.android.library") version "8.1.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.10" apply false
+}
+
 val newBuildDir: Directory =
     rootProject.layout.buildDirectory
         .dir("../../build")
@@ -14,6 +20,23 @@ rootProject.layout.buildDirectory.value(newBuildDir)
 subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
+    
+    // Fix compileSdk for dependencies like path_provider_android
+    pluginManager.withPlugin("com.android.library") {
+        configure<com.android.build.gradle.LibraryExtension> {
+            if (compileSdk == 0) {
+                compileSdk = 34
+            }
+        }
+    }
+    
+    pluginManager.withPlugin("com.android.application") {
+        configure<com.android.build.gradle.AppExtension> {
+            if (compileSdk == 0) {
+                compileSdk = 34
+            }
+        }
+    }
 }
 subprojects {
     project.evaluationDependsOn(":app")
